c8b78b99821a68006c82ac0c6c42f99829542ef6
Wayne Grimes Hirakawa <himobileappdev@gmail.com>
2014-05-06 11:36:13 -1000

    Added Entertainment Tab

--- Resources/app.js
+++ Resources/app.js
@@ -1,11 +1,8 @@
 (function(){
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
-
 // create tab group
 var tabGroup = Titanium.UI.createTabGroup();
-
-
 //
 // create base UI tab and root window
 //
@@ -27,20 +24,25 @@ var activitiesWindow = new SectionWindow('Activities');
 var tab2 = Titanium.UI.createTab({  
     icon:'63-runner.png',
     title:'Activites',
-    window:activitiesWindow
+    window: activitiesWindow
 });
 
 activitiesWindow.containingTab = tab2;
+
+var entertainmentWindow = new SectionWindow('Entertainment');
+var tab3 = Titanium.UI.createTab({  
+    icon:'63-runner.png',
+    title:'Entertainment',
+    window: entertainmentWindow
+});
+
+entertainmentWindow.containingTab = tab3;
 //
 //  add tabs
 //
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
+tabGroup.addTab(tab1);
+tabGroup.addTab(tab2);
+tabGroup.addTab(tab3);
 // open tab group
 tabGroup.open();
 })();
-
-
-
